00dd0f069e6fc76fb7fa8969757c708ab11a56cd
ebuaa <aubea@axel-aube.fr>
2025-06-17 10:10:57 +0200

    [FIX] Error when dump

--- db-dump.sh
+++ db-dump.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "📦 Creating a dump of the videgrenier database..."
+echo "Creating a dump of the videgrenier database..."
 
-docker exec db-container \
-  mysqldump -u vguser -pvgpassword videgrenier > ./www/sql/dump.sql
+docker exec dev-db-container mariadb-dump -u vguser -pvgpassword videgrenier > ./www/sql/dump.sql
 
-echo "✅ Dump saved to ./www/sql/dump.sql"
+echo "Dump saved to ./www/sql/dump.sql"
